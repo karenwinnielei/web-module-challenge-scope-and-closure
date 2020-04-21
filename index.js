4ff79dd242b5a14254b7227c33a8516fd3c733a4
Karen Lei <karenwinnielei@gmail.com>
2020-04-21 16:08:28 -0700

    complete task 3

--- index.js
+++ index.js
@@ -84,11 +84,34 @@ finalScore(inning, 9) might return:
 
 */ 
 
-function finalScore(/*code Here*/){
+// function finalScore(inning, number){
+//   for(let i = 0; i < number; i++)
+//   let homeScore = inning() * Math.round(Math.random()*number);
+//   let awayScore = inning() * Math.round(Math.random()*number);
+//   let score = {
+//     "Home": `${homeScore}`,
+//     "Away": `${awayScore}`,
+//   }
+//   return score;
+// }  
+// console.log(finalScore(inning, 9));
 
-  /*Code Here*/
-
+function finalScore(cb, number){
+  let finalScore = {
+    "Home": 0,
+    "Away": 0,
+  }
+  for(let i = 0; i < number; i++){
+    // if(i % 2 === 0){
+      finalScore.Home = finalScore.Home += cb();
+    // }else{
+      finalScore.Away = finalScore.Away += cb();
+      console.log(finalScore.Away);
+    // }
+  }
+  return finalScore;
 }
+console.log(finalScore(inning, 5));
 
 /* Task 4: 
 
@@ -112,8 +135,92 @@ and returns the score at each pont in the game, like so:
 
 Final Score: awayTeam - homeTeam */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function numberToOrdinal(n) {
+  var j = n % 10;
+       k = n % 100;
+ 
+   if (j == 0 && n == 0) {
+       return 0;
+ }
+ 
+   if (j == n && k != 11) {
+       return n + "st";
+   }
+   if (j == 2 && k != 12) {
+       return n + "nd";
+   }
+   if (j == 3 && k != 13) {
+       return n + "rd";
+   }
+   return n + "th";
 }
+function scoreboard(callback, num){
+  let finalScore = {
+    "Home": 0,
+    "Away": 0,
+  }
+  for(let i = 0; i < num; i++){
+    // if(i % 2 === 0){
+      finalScore.Home = finalScore.Home += callback();
+      console.log(`${numberToOrdinal(i)} inning: ${finalScore.Away} - ${finalScore.Home}`);
+      finalScore.Away = finalScore.Away += callback();
+    // }
+  }
+  return finalScore;
+}
+console.log(scoreboard(inning, 5));
 
 
+
+// function getInningScore(inning, number){
+//   let homeTeam = [];
+//   homeTeam.push(inning());  
+// }
+// getInningScore(inning(), 5)
+// console.log(getInningScore());
+
+// function scoreboard(num,cb) {
+
+//   return function getInningScore(){
+
+//     console.log( 
+//     `1st inning: awayTeam - homeTeam
+//     2nd inning: awayTeam - homeTeam
+//     3rd inning: awayTeam - homeTeam
+//     4th inning: awayTeam - homeTeam
+//     5th inning: awayTeam - homeTeam
+//     6th inning: awayTeam - homeTeam
+//     7th inning: awayTeam - homeTeam
+//     8th inning: awayTeam - homeTeam
+//     9th inning: awayTeam - homeTeam`)
+
+//     return function inning(){
+
+//     }
+//   }
+// }
+
+// function inning(repeat){
+//   var inningCount = 1
+
+  function getInningScore(inning, number){
+    let awayTeam = inning()*number; 
+    let homeTeam = inning()*number;
+    console.log(inning(number))
+
+    function scoreboard(){
+
+    }
+  }
+// }
+
+// let homeTeam = gettingInningScore();
+  // return `1st inning: awayTeam - homeTeam
+  // 2nd inning: awayTeam - homeTeam
+  // 3rd inning: awayTeam - homeTeam
+  // 4th inning: awayTeam - homeTeam
+  // 5th inning: awayTeam - homeTeam
+  // 6th inning: awayTeam - homeTeam
+  // 7th inning: awayTeam - homeTeam
+  // 8th inning: awayTeam - homeTeam
+  // 9th inning: awayTeam - homeTeam
